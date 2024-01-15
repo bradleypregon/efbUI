a879b75c945e3d6d93acabf779ab4c16c1f42698
bradleypregon <bradley.pregon@gmail.com>
2024-01-15 15:29:56 -0600

    Pull-down functionality and simbrief

--- efbUI/TopBar/TopBarView.swift
+++ efbUI/TopBar/TopBarView.swift
@@ -7,66 +7,110 @@
 
 import SwiftUI
 import CoreLocation
+import SwiftData
 
 struct TopBarView: View {
   private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-  @State private var currentZuluTime: String = ""
+  @State private var currentZuluTime24: String = ""
   @State private var currentTime: String = ""
-  @EnvironmentObject var simConnect: SimConnect
+  @Environment(SimConnect.self) var simConnect
+  
+  @State private var dragOffset: CGFloat = 40
+  @State private var expanded: Bool = false
+  
+  @Query var simbriefUser: [SimBriefUser]
   
   var body: some View {
-    HStack {
-      Spacer()
-      
-      Text("Connect:")
-      Button {
-        let server = SimConnectServer(simConnect: simConnect)
-        try! server.start()
-      } label: {
-        Image(systemName: "target")
+    VStack {
+      HStack {
+        Spacer()
+        
+        Text("Connect:")
+        Button {
+          let server = SimConnectServer(simConnect: simConnect)
+          
+          do {
+            try server.start()
+          } catch let error {
+            print("Error trying to start SimConnect server: \(error)")
+          }
+        } label: {
+          Image(systemName: "target")
+            .frame(width: 25, height: 25, alignment: .center)
+            .font(.title)
+        }
+        
+        Spacer()
+          .frame(width: 40)
+        Text("Hdg: \(simConnect.simConnectShip?.heading.string ?? "")")
+        Text("Alt: \(simConnect.simConnectShip?.altitude.string ?? "")")
+        Text("Spd: \(simConnect.simConnectShip?.speed.string ?? "")")
+        Spacer()
+        Text(currentZuluTime24)
+        Spacer()
       }
-      .frame(width: 40, height: 40, alignment: .center)
-      .font(.title)
-      
-//      Spacer()
-//      Text("Hdg: \(String(format: "%.1f", simConnect.gpsEvent?.heading ?? 0.0))")
-//      Text("Alt: \(String(format: "%.1f", simConnect.gpsEvent?.altitude ?? 0.0))")
-//
-//      Text("Spd: \(String(format: "%.0f", simConnect.gpsEvent?.speed ?? 0.0))")
-      Text("Hdg: \(String(format: "%.1f", simConnect.simConnectShip?.heading ?? .zero))")
-      Text("Alt: \(String(format: "%.1f", simConnect.simConnectShip?.altitude ?? .zero))")
-      Text("Spd: \(String(format: "%.0f", simConnect.simConnectShip?.speed ?? .zero))")
+//      .fixedSize(horizontal: false, vertical: true)
       Spacer()
-      Text(currentTime)
-      Text(currentZuluTime)
+      // hidden content here
+      if expanded {
+        VStack {
+          if let simbriefID = simbriefUser.first?.userID {
+            HStack {
+              Text("Simbrief ID: \(simbriefID)")
+              Button {
+                let simbriefAPI = SimBriefAPI()
+                simbriefAPI.fetchLastFlightPlan(for: simbriefID)
+              } label: {
+                Text("Fetch SimBrief Route")
+              }
+            }
+          }
+          
+        }
+      }
       Spacer()
-      Spacer()
+      VStack {
+        RoundedRectangle(cornerRadius: 10)
+          .frame(width: 200, height: 5)
+          .foregroundStyle(.gray)
+      }
+      .offset(x: 0, y: -5)
+      .gesture(
+        DragGesture()
+          .onChanged { value in
+            dragOffset = value.translation.height
+          }
+          .onEnded { _ in
+            withAnimation {
+              if dragOffset > 75 {
+                dragOffset = 300
+                expanded = true
+              }
+              else {
+                dragOffset = 40
+                expanded = false
+              }
+            }
+          }
+      )
     }
+    .frame(height: dragOffset, alignment: .top)
     .onReceive(timer) { _ in
-      getCurrentZuluTime()
-      getCurrentTime()
+      getCurrentZuluTime24()
     }
   }
   
-  func getCurrentZuluTime() {
+  func getCurrentZuluTime24() {
     let df = DateFormatter()
     df.dateFormat = "HH:mm"
     df.timeZone = TimeZone(identifier: "UTC")
     let currentDate = Date()
     let zulu = df.string(from: currentDate)
-    currentZuluTime = "Zulu: \(zulu)z"
-  }
-  
-  func getCurrentTime() {
-    let df = DateFormatter()
-    df.dateStyle = .none
-    df.timeStyle = .short
-    let currentDate = df.string(from: Date())
-    currentTime = "Current: \(currentDate)"
+    currentZuluTime24 = "\(zulu)z"
   }
   
 }
 
-//#Preview {
-//  TopBarView()
-//}
+#Preview {
+  TopBarView()
+}
