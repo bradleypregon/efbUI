22458b3eb7bf31cad1fc17460d9c4cc3f9d669e4
bradleypregon <bradley.pregon@gmail.com>
2024-01-15 23:09:29 -0600

    Temp pulldown fix, testing SimBrief data fetch

--- efbUI/TopBar/TopBarView.swift
+++ efbUI/TopBar/TopBarView.swift
@@ -9,6 +9,53 @@ import SwiftUI
 import CoreLocation
 import SwiftData
 
+// Linked list
+// Each node can be an airport, vor, navaid, departure, arrival, any type of navigation aid with a coordinate
+// upon simbrief api fetch, compile route and load it into LinkedList
+
+// Typical route format:
+// head -> Airport
+// nodes -> each route component
+// tail -> Airport
+
+class LinkedList<T> {
+  var head: Node<T>?
+  
+  func append(value: T) {
+    let newNode = Node(value: value)
+    if head == nil {
+      head = newNode
+    } else {
+      var current = head
+      while current?.next != nil {
+        current = current?.next
+      }
+      current?.next = newNode
+    }
+  }
+  
+  func printList() {
+    var current = head
+    while current != nil {
+      if current?.next != nil {
+        print(current!.value, terminator: " -> ")
+      } else {
+        print(current!.value)
+      }
+      current = current?.next
+    }
+  }
+}
+
+class Node<T> {
+  var value: T
+  var next: Node?
+  
+  init(value: T) {
+    self.value = value
+  }
+}
+
 struct TopBarView: View {
   private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
   @State private var currentZuluTime24: String = ""
@@ -19,7 +66,49 @@ struct TopBarView: View {
   @State private var expanded: Bool = false
   
   @Query var simbriefUser: [SimBriefUser]
+  @State var route: String = ""
   
+  /*
+   @State var dragOffset: CGFloat = 40
+   @State var expanded: Bool = false
+   var body: some View {
+    VStack {
+      HStack {
+        // a small bit of content here at the top
+      }
+   
+      if expanded {
+        // more content here when the view is expanded
+      }
+   
+      VStack {
+        RoundedRectangle(cornerRadius: 10)
+          .frame(width: 200, height: 5)
+      }
+      .offset(x: 0, y: -5)
+      .gesture(
+        DragGesture()
+          .onChanged { value in
+            dragOffset = value.translation.height
+          }
+          .onEnded { _ in
+            withAnimation {
+              if dragOffset > 75 {
+                dragOffset = 300
+                expanded = true
+              }
+              else {
+                dragOffset = 0
+                expanded = false
+              }
+            }
+          }
+        )
+    }
+    .frame(height: dragOffset)
+   }
+   
+   */
   var body: some View {
     VStack {
       HStack {
@@ -55,14 +144,28 @@ struct TopBarView: View {
       if expanded {
         VStack {
           if let simbriefID = simbriefUser.first?.userID {
+            Text("SimBrief OFP")
             HStack {
-              Text("Simbrief ID: \(simbriefID)")
+              VStack {
+                Text("SimBrief ID")
+                Text(simbriefID)
+              }
               Button {
                 let simbriefAPI = SimBriefAPI()
-                simbriefAPI.fetchLastFlightPlan(for: simbriefID)
+                simbriefAPI.fetchLastFlightPlan(for: simbriefID) { ofp in
+                  route = "\(ofp.origin.icaoCode) \(ofp.general.routeNavigraph) \(ofp.destination.icaoCode)"
+                  let tempSplit = route.split(separator: " ")
+                  let linkedList = LinkedList<String>()
+                  for item in tempSplit {
+                    linkedList.append(value: String(item))
+                  }
+                  linkedList.printList()
+                }
               } label: {
-                Text("Fetch SimBrief Route")
+                Text("Fetch Route")
               }
+              
+              Text(route)
             }
           }
           
@@ -75,26 +178,39 @@ struct TopBarView: View {
           .foregroundStyle(.gray)
       }
       .offset(x: 0, y: -5)
-      .gesture(
-        DragGesture()
-          .onChanged { value in
-            dragOffset = value.translation.height
+      .onTapGesture {
+        withAnimation {
+          if !expanded {
+            dragOffset = 300
+            expanded = true
+          } else {
+            dragOffset = 40
+            expanded = false
           }
-          .onEnded { _ in
-            withAnimation {
-              if dragOffset > 75 {
-                dragOffset = 300
-                expanded = true
-              }
-              else {
-                dragOffset = 40
-                expanded = false
-              }
-            }
-          }
-      )
+        }
+      }
+      // MARK: TODO
+      // TODO: Fix drag gesture. Pulling down works ok, pushing up is funky
+//      .gesture(
+//        DragGesture()
+//          .onChanged { value in
+//            dragOffset = value.translation.height
+//          }
+//          .onEnded { _ in
+//            withAnimation {
+//              if dragOffset > 75 {
+//                dragOffset = 300
+//                expanded = true
+//              }
+//              else {
+//                dragOffset = 40
+//                expanded = false
+//              }
+//            }
+//          }
+//      )
     }
-    .frame(height: dragOffset, alignment: .top)
+    .frame(height: dragOffset)
     .onReceive(timer) { _ in
       getCurrentZuluTime24()
     }
