132116d77d9ae1b06e214c54e1926f91463f08f1
bradleypregon <bradley.pregon@gmail.com>
2024-02-03 19:28:11 -0600

    performance fixes for loading

--- efbUI/Map/AirportData/AirportJSONModel.swift
+++ efbUI/Map/AirportData/AirportJSONModel.swift
@@ -9,52 +9,72 @@ import Foundation
 import SwiftUI
 @_spi(Experimental) import MapboxMaps
 
-class AirportJSONModel {
+final class AirportJSONModel {
+  let path = Bundle.main.path(forResource: "Airports", ofType: "geojson")
+  var airports: Set<Airport> = Set<Airport>()
   
-  let jsonFile = "Airports"
-  let jsonFileType = "geojson"
-  
-  func fetchGeoJSON(size: String, bounds: CoordinateBounds) -> [Airport] {
-    if let path = Bundle.main.path(forResource: jsonFile, ofType: jsonFileType) {
-      
+  init() {
+    if let path {
       do {
         let data = try Data(contentsOf: URL(fileURLWithPath: path))
-        let filteredAirports = try JSONDecoder().decode([Airport].self, from: data)
-          .filter { airport in
-            let airportSize = airport.properties.size.rawValue
-            return airportSize == size || AirportSize(rawValue: airportSize)! < AirportSize(rawValue: size)!
-          }
-          .filter { airport in
-            bounds.contains(
-              forPoint: CLLocationCoordinate2D(latitude: airport.coordinates.lat, longitude: airport.coordinates.long),
-              wrappedCoordinates: true
-            )
-          }
-        
-        return filteredAirports
-      } catch let error {
-        print("Error decoding Airport JSON from Airport.GeoJSON: \(error)")
-        return []
+        let temp = try JSONDecoder().decode([Airport].self, from: data)
+        airports = Set(temp)
+      } catch {
+        print("Error decoding Airport JSON from Airports.GeoJSON: \(error)")
       }
     }
-    return []
+    
   }
   
-}
-
-enum AirportSize: String, Comparable {
-  case Small
-  case Medium
-  case Large
+  func fetchGeoJSON(size: String, bounds: CoordinateBounds) -> [Airport] {
+    let filter = airports
+      .filter { airport in
+        let airportSize = airport.properties.size.rawValue
+        return airportSize == size || AirportSize(rawValue: airportSize)! < AirportSize(rawValue: size)!
+      }
+      .filter { airport in
+        bounds.contains(
+          forPoint: CLLocationCoordinate2D(latitude: airport.coordinates.lat, longitude: airport.coordinates.long),
+          wrappedCoordinates: false
+        )
+      }
+    return Array(filter)
+//    if let path {
+//      do {
+//        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+//        let filteredAirports = try JSONDecoder().decode([Airport].self, from: data)
+//          .filter { airport in
+//            let airportSize = airport.properties.size.rawValue
+//            return airportSize == size || AirportSize(rawValue: airportSize)! < AirportSize(rawValue: size)!
+//          }
+//          .filter { airport in
+//            bounds.contains(
+//              forPoint: CLLocationCoordinate2D(latitude: airport.coordinates.lat, longitude: airport.coordinates.long),
+//              wrappedCoordinates: true
+//            )
+//          }
+//        return filteredAirports
+//      } catch let error {
+//        print("Error decoding Airport JSON from Airport.GeoJSON: \(error)")
+//        return []
+//      }
+//    }
+//    return []
+  }
   
-  // Establishing order among sizes for comparison
-  static func < (lhs: AirportSize, rhs: AirportSize) -> Bool {
-    // Define the hierarchy of sizes based on their raw values
-    let sizeOrder: [AirportSize] = [.Large, .Medium, .Small]
-    guard let lhsIndex = sizeOrder.firstIndex(of: lhs),
-          let rhsIndex = sizeOrder.firstIndex(of: rhs) else {
-      return false
+  enum AirportSize: String, Comparable {
+    case Small
+    case Medium
+    case Large
+    
+    static func < (lhs: AirportSize, rhs: AirportSize) -> Bool {
+      let sizeOrder: [AirportSize] = [.Large, .Medium, .Small]
+      guard let lhsIndex = sizeOrder.firstIndex(of: lhs),
+            let rhsIndex = sizeOrder.firstIndex(of: rhs) else {
+        return false
+      }
+      return lhsIndex < rhsIndex
     }
-    return lhsIndex < rhsIndex
   }
+  
 }
