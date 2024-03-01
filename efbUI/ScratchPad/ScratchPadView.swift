51eeef648af0ce67cf0b1f4b34c8be2a563e3ab4
bradleypregon <bradley.pregon@gmail.com>
2024-03-01 17:39:24 -0600

    Using SwiftUI views instead of Images for ATIS and CRAFT

--- efbUI/ScratchPad/ScratchPadView.swift
+++ efbUI/ScratchPad/ScratchPadView.swift
@@ -9,55 +9,94 @@ import SwiftUI
 import PencilKit
 
 struct ScratchPadView: View {
-  
-  @State private var selectedScreenIndex = 0
-  
   @State var atisCanvas = PKCanvasView()
   @State var craftCanvas = PKCanvasView()
   @State var scratchCanvas = PKCanvasView()
   
-  var tabs = ["ATIS", "CRAFT", "Blank"]
+  private enum ScratchPadTabs: String, CaseIterable, Identifiable {
+    case ATIS, CRAFT, Blank
+    var id: Self { self }
+  }
+  
+  @State private var selectedTab: ScratchPadTabs = .ATIS
   
   var body: some View {
     VStack {
-      Spacer()
-      Divider()
-      
-      // Horizontal Icons
       HStack {
-        ForEach(0..<3, id: \.self) { number in
-          Spacer()
-          Button (action: {
-            self.selectedScreenIndex = number
-          }, label: {
-            Text(tabs[number])
-              .font(.system(size: 20, weight: .light, design: .rounded))
-              .foregroundColor(self.selectedScreenIndex == number ? .black : Color(UIColor.lightGray))
-          })
-          Spacer()
+        /// Picker
+        Picker("ScrachPad", selection: $selectedTab) {
+          ForEach(ScratchPadTabs.allCases) { tab in
+            Text(tab.rawValue)
+          }
         }
-        Spacer()
-        Button (action: {
-          //
-        }, label: {
-          Image(systemName: "list.bullet.circle")
-            .font(.title)
-        })
-        Spacer()
+        .pickerStyle(.segmented)
+        
+        /// PencilKit control buttons
+        HStack(spacing: 20) {
+          Button {
+            switch selectedTab {
+            case .ATIS:
+              atisCanvas.tool = PKInkingTool(.pen)
+            case .CRAFT:
+              craftCanvas.tool = PKInkingTool(.pen)
+            case .Blank:
+              scratchCanvas.tool = PKInkingTool(.pen)
+            }
+          } label: {
+            Image(systemName: "pencil")
+              .font(.title)
+          }
+          .buttonStyle(.bordered)
+          .frame(maxHeight: .infinity)
+          
+          Button {
+            switch selectedTab {
+            case .ATIS:
+              atisCanvas.tool = PKEraserTool(.bitmap)
+            case .CRAFT:
+              craftCanvas.tool = PKEraserTool(.bitmap)
+            case .Blank:
+              scratchCanvas.tool = PKEraserTool(.bitmap)
+            }
+          } label: {
+            Image(systemName: "eraser")
+              .font(.title)
+          }
+          .buttonStyle(.bordered)
+          .frame(maxHeight: .infinity)
+          
+          
+          Button {
+            switch selectedTab {
+            case .ATIS:
+              atisCanvas.drawing.strokes.removeAll()
+            case .CRAFT:
+              craftCanvas.drawing.strokes.removeAll()
+            case .Blank:
+              scratchCanvas.drawing.strokes.removeAll()
+            }
+          } label: {
+            Text("Clear")
+              .font(.title)
+          }
+          .buttonStyle(.bordered)
+          .frame(maxHeight: .infinity)
+          
+        }
+        .padding([.leading, .trailing], 40)
       }
-      //Divider()
-      // Controlling each button
+        .frame(height: 70)
+      
       ZStack {
-        switch selectedScreenIndex {
-        case 0:
-          Image("ATIS")
-            .resizable()
+        switch selectedTab {
+        case .ATIS:
+          ScratchPadATIS()
           DrawingView(canvas: $atisCanvas)
-        case 1:
-          Image("CRAFT")
-            .resizable()
+        case .CRAFT:
+          ScratchPadCRAFT()
           DrawingView(canvas: $craftCanvas)
         default:
+          Color.Neumorphic.main
           DrawingView(canvas: $scratchCanvas)
         }
         
