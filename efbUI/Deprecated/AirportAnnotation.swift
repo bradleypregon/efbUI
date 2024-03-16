377b4b28f9184d820189476c168bb98c42bd56f2
bradleypregon <bradley.pregon@gmail.com>
2024-03-16 16:45:10 -0500

    Deprecation

--- efbUI/Deprecated/AirportAnnotation.swift
+++ efbUI/Deprecated/AirportAnnotation.swift
@@ -5,20 +5,20 @@
 //  Created by Bradley Pregon on 1/8/22.
 //
 
-import CoreLocation
-import MapKit
+//import CoreLocation
+//import MapKit
 
-class AirportAnnotation: NSObject, Identifiable, MKAnnotation {
-  // TODO: Make unique?
-  var id = UUID()
-  
-  var coordinate: CLLocationCoordinate2D
-  let properties: Properties
-  
-  init(id: UUID = UUID(), coordinate: CLLocationCoordinate2D, properties: Properties) {
-    self.id = id
-    self.coordinate = coordinate
-    self.properties = properties
-  }
-  
-}
+//class AirportAnnotation: NSObject, Identifiable, MKAnnotation {
+//
+//  var id = UUID()
+//  
+//  var coordinate: CLLocationCoordinate2D
+//  let properties: Properties
+//  
+//  init(id: UUID = UUID(), coordinate: CLLocationCoordinate2D, properties: Properties) {
+//    self.id = id
+//    self.coordinate = coordinate
+//    self.properties = properties
+//  }
+//  
+//}
