53435fa380f54261c1d2afe303fececcf47c3c22
bradleypregon <bradley.pregon@gmail.com>
2024-03-17 21:47:34 -0500

    Moved custom WrappingHStack to own file for cleanliness

---
+++ efbUI/Extra/WrappingHStack.swift
@@ -0,0 +1,75 @@
+//
+//  WrappingHStack.swift
+//  efbUI
+//
+//  Created by Bradley Pregon on 3/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+// Adapted from: https://stackoverflow.com/questions/62102647/swiftui-hstack-with-wrap-and-dynamic-height/62103264#62103264
+struct WrappingHStack<Model, V>: View where Model: Hashable, V: View {
+  typealias ViewGenerator = (Model) -> V
+  
+  var models: [Model]
+  var viewGenerator: ViewGenerator
+  var horizontalSpacing: CGFloat = 4
+  var verticalSpacing: CGFloat = 4
+  
+  @State private var totalHeight = CGFloat.zero
+  
+  var body: some View {
+    VStack {
+      GeometryReader { geometry in
+        self.generateContent(in: geometry)
+      }
+    }
+    .frame(height: totalHeight)
+  }
+  
+  private func generateContent(in geometry: GeometryProxy) -> some View {
+    var width = CGFloat.zero
+    var height = CGFloat.zero
+    
+    return ZStack(alignment: .topLeading) {
+      ForEach(self.models, id: \.self) { models in
+        viewGenerator(models)
+          .padding(.horizontal, horizontalSpacing)
+          .padding(.vertical, verticalSpacing)
+          .alignmentGuide(.leading, computeValue: { dimension in
+            if (abs(width - dimension.width) > geometry.size.width) {
+              width = 0
+              height -= dimension.height
+            }
+            let result = width
+            if models == self.models.last! {
+              width = 0 //last item
+            } else {
+              width -= dimension.width
+            }
+            return result
+          })
+          .alignmentGuide(.top, computeValue: { dimension in
+            let result = height
+            if models == self.models.last! {
+              height = 0 // last item
+            }
+            return result
+          })
+      }
+      
+    }.background(viewHeightReader($totalHeight))
+  }
+  
+  private func viewHeightReader(_ binding: Binding<CGFloat>) -> some View {
+    return GeometryReader { geometry -> Color in
+      let rect = geometry.frame(in: .local)
+      DispatchQueue.main.async {
+        binding.wrappedValue = rect.size.height
+      }
+      return .clear
+    }
+  }
+}
+
