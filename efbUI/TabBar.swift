61f6aad98b939c361c01a7dfbe7b671aa14cbad6
bradleypregon <bradley.pregon@gmail.com>
2024-05-20 20:19:37 -0500

    Tweaks

--- efbUI/TabBar.swift
+++ efbUI/TabBar.swift
@@ -19,7 +19,7 @@ struct TabBar: View {
   @State private var tenMileInbd: Bool = false
   @State private var fiveMileInbd: Bool = false
   
-  let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
+  private let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
   
   var body: some View {
     TabView(selection: $selectedTab) {
@@ -62,34 +62,35 @@ struct TabBar: View {
         .tag(5)
     }
     .onReceive(timer) { _ in
-      if let ofp = simbrief.ofp {
-        let shipLocation = CLLocation(latitude: ship.ownship.coordinate.latitude, longitude: ship.ownship.coordinate.longitude)
-        let originCoord = CLLocation(latitude: Double(ofp.origin.posLat) ?? .zero, longitude: Double(ofp.origin.posLong) ?? .zero)
-        let destCoord = CLLocation(latitude: Double(ofp.destination.posLat) ?? .zero, longitude: Double(ofp.destination.posLong) ?? .zero)
-        
-        // if we are 20 miles past origin airport, notification for final call
-        if (shipLocation.distance(from: originCoord) * 0.000621371 >= 20) && depNoticeTriggered == false {
-          Toast.shared.present(title: "20mi from \(ofp.origin.icaoCode): Final Call", symbol: "airplane.departure")
-          depNoticeTriggered = true
-        }
-        
-        // if we are 30 miles inbound to de stinatino, notification for inbound
-        if shipLocation.distance(from: destCoord) * 0.000621371 <= 30 && inboundTriggered == false  {
-          Toast.shared.present(title: "30mi from \(ofp.destination.icaoCode): Inbound Call", symbol: "airplane.arrival")
-          inboundTriggered = true
-        }
-        
-        // 10 miles, make another call, 10 miles out
-        if shipLocation.distance(from: destCoord) * 0.000621371 <= 10 && tenMileInbd == false {
-          Toast.shared.present(title: "10mi from \(ofp.destination.icaoCode): 10 Mile Call", symbol: "airplane.arrival")
-          tenMileInbd = true
-        }
-        
-        // 5 miles, make another call
-        if shipLocation.distance(from: destCoord) * 0.000621371 <= 5 && fiveMileInbd == false {
-          Toast.shared.present(title: "5mi from \(ofp.destination.icaoCode): 5 Mile Call", symbol: "airplane.arrival")
-          fiveMileInbd = true
-        }
+      guard let ofp = simbrief.ofp else { return }
+      guard ship.ownship.coordinate.latitude != .zero else { return }
+      
+      let shipLocation = CLLocation(latitude: ship.ownship.coordinate.latitude, longitude: ship.ownship.coordinate.longitude)
+      let originCoord = CLLocation(latitude: Double(ofp.origin.posLat) ?? .zero, longitude: Double(ofp.origin.posLong) ?? .zero)
+      let destCoord = CLLocation(latitude: Double(ofp.destination.posLat) ?? .zero, longitude: Double(ofp.destination.posLong) ?? .zero)
+      
+      // if we are 20 miles past origin airport, notification for final call
+      if (shipLocation.distance(from: originCoord) * 0.000621371 >= 20) && depNoticeTriggered == false {
+        Toast.shared.present(title: "20mi from \(ofp.origin.icaoCode): Final Call", symbol: "airplane.departure")
+        depNoticeTriggered = true
+      }
+      
+      // if we are 30 miles inbound to de stinatino, notification for inbound
+      if shipLocation.distance(from: destCoord) * 0.000621371 <= 30 && inboundTriggered == false  {
+        Toast.shared.present(title: "30mi from \(ofp.destination.icaoCode): Inbound Call", symbol: "airplane.arrival")
+        inboundTriggered = true
+      }
+      
+      // 10 miles, make another call, 10 miles out
+      if shipLocation.distance(from: destCoord) * 0.000621371 <= 10 && tenMileInbd == false {
+        Toast.shared.present(title: "10mi from \(ofp.destination.icaoCode): 10 Mile Call", symbol: "airplane.arrival")
+        tenMileInbd = true
+      }
+      
+      // 5 miles, make another call
+      if shipLocation.distance(from: destCoord) * 0.000621371 <= 5 && fiveMileInbd == false {
+        Toast.shared.present(title: "5mi from \(ofp.destination.icaoCode): 5 Mile Call", symbol: "airplane.arrival")
+        fiveMileInbd = true
       }
     }
   }
