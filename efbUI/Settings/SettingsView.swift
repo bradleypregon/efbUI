dacf34af3b02590bd2e042117cd985f04a3cba93
bradleypregon <bradley.pregon@gmail.com>
2024-05-25 11:46:40 -0500

    New feature: celsius/fahrenheit converter

--- efbUI/Settings/SettingsView.swift
+++ efbUI/Settings/SettingsView.swift
@@ -108,6 +108,10 @@ struct SettingsView: View {
             HStack {
               TextField("Celsius/Fahrenheitº", value: $calcInput, format: .number, prompt: Text("C/Fº"))
                 .textFieldStyle(.roundedBorder)
+<<<<<<< HEAD
+=======
+                .keyboardType(.decimalPad)
+>>>>>>> aacbb17 (New feature: celsius/fahrenheit converter)
                 .onChange(of: calcInput) {
                   if let calc = calcInput {
                     calcFOutput = (Double(calc)*1.8) + 32
