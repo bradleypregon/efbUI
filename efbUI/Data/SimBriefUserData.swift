61acf7e752cba6df36fb0ffe5de02a49862c06f0
bradleypregon <bradley.pregon@gmail.com>
2024-05-25 07:51:04 -0500

    Updated model for SwiftData

--- efbUI/Data/SimBriefUserData.swift
+++ efbUI/Data/SimBriefUserData.swift
@@ -8,10 +8,18 @@
 import SwiftData
 
 @Model
-final class SimBriefUser {
-  var userID: String
+final class UserSettings {
+  var simbriefUserID: String?
+  var ownshipRegistration: String?
+  var outboundDistance: Int
+  var inboundDistance: Int
+  var finalDistance: Int
   
-  init(userID: String) {
-    self.userID = userID
+  init(simbriefUserID: String? = nil, ownshipRegistration: String? = nil, outboundDistance: Int = 20, inboundDistance: Int = 20, finalDistance: Int = 5) {
+    self.simbriefUserID = simbriefUserID
+    self.ownshipRegistration = ownshipRegistration
+    self.outboundDistance = outboundDistance
+    self.inboundDistance = inboundDistance
+    self.finalDistance = finalDistance
   }
 }
