ef4400d63ce6f43e992dcb9d96fbe7b23641f619
bradleypregon <bradley.pregon@gmail.com>
2024-10-30 20:15:25 -0500

    wip: adding different radar to map with better api fetch implementation

--- efbUI/Radar/RainviewerAPI.swift
+++ efbUI/Radar/RainviewerAPI.swift
@@ -9,6 +9,18 @@ import Foundation
 
 class RainviewerAPI {
   
+  func fetchRadar() async throws -> RainviewerSchema {
+    guard let url = URL(string: "https://api.rainviewer.com/public/weather-maps.json") else {
+      fatalError("Invalid Rainviewer API URL")
+    }
+    let request = URLRequest(url: url)
+    let (data, response) = try await URLSession.shared.data(for: request)
+    
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching Rainviewer API Data")}
+    let rainviewerData = try JSONDecoder().decode(RainviewerSchema.self, from: data)
+    return rainviewerData
+  }
+  
   func fetchRadar(completion: @escaping (RainviewerSchema) -> ()) {
     let url = "https://api.rainviewer.com/public/weather-maps.json"
     guard let url = URL(string: url) else {
