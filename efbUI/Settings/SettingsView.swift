c9c9df17c7536168184124ed511c944fd5116ad5
bradleypregon <bradley.pregon@gmail.com>
2024-11-05 22:09:45 -0600

    Display device current IPv4 Opting to do this versus configuring UDP Broadcast, which is not available without Apple Developer.

--- efbUI/Settings/SettingsView.swift
+++ efbUI/Settings/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import MapKit
 import Observation
 import SwiftData
+import SystemConfiguration.CaptiveNetwork
 
 struct SettingsView: View {
   @Environment(\.modelContext) private var modelContext
@@ -99,7 +100,7 @@ struct SettingsView: View {
             } header: {
               Text("Misc Settings")
             }
-
+            
           }
         }
         VStack {
@@ -122,6 +123,10 @@ struct SettingsView: View {
             Text("\(String(format: "%.1f", calcFOutput))ºF")
             Text("\(String(format: "%.1f", calcCOutput))ºC")
           }
+          VStack {
+            Text("Device IP")
+            Text(getDeviceIPAddress())
+          }
         }
       }
       
@@ -135,5 +140,32 @@ struct SettingsView: View {
       inboundNotificationDistance = userSettings.first?.inboundDistance ?? 20
       finalNotificationDistance = userSettings.first?.finalDistance ?? 5
     }
+  }
+  
+  func getDeviceIPAddress() -> String {
+    var address: String = ""
+    var ifaddr: UnsafeMutablePointer<ifaddrs>?
+    
+    if getifaddrs(&ifaddr) == 0 {
+      var ptr = ifaddr
+      while ptr != nil {
+        let interface = ptr?.pointee
+        let addrFamily = interface?.ifa_addr.pointee.sa_family
+        if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+          if let name = String(validatingUTF8: interface!.ifa_name), name == "en0" {
+            var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+            if getnameinfo(interface!.ifa_addr, socklen_t(interface!.ifa_addr.pointee.sa_len),
+                           &hostname, socklen_t(hostname.count),
+                           nil, socklen_t(0), NI_NUMERICHOST) == 0 {
+              address = String(cString: hostname)
+            }
+          }
+        }
+        ptr = ptr?.pointee.ifa_next
+      }
+      freeifaddrs(ifaddr)
+    }
+    
+    return address
   }
 }
