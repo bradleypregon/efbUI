682aa231716c69d7a8f7b575e6bc47349c8cc8be
bradleypregon <bradley.pregon@gmail.com>
2024-11-11 21:28:38 -0600

    better rainviewer api error handling

--- efbUI/Radar/RainviewerAPI.swift
+++ efbUI/Radar/RainviewerAPI.swift
@@ -7,16 +7,39 @@
 
 import Foundation
 
+enum RainviewerAPIError: Error, LocalizedError {
+  case badURL
+  case badResponse
+  case badDecode
+  
+  var errorDescription: String? {
+    switch self {
+    case .badURL:
+      return "Rainviewer API Error: Invalid URL."
+    case .badResponse:
+      return "Rainviewer API Error: Invalid Response."
+    case .badDecode:
+      return "Rainviewer API Error: Invalid Decode."
+    }
+  }
+}
+
 class RainviewerAPI {
   func fetchRadar() async throws -> RainviewerSchema {
     guard let url = URL(string: "https://api.rainviewer.com/public/weather-maps.json") else {
-      fatalError("Invalid Rainviewer API URL")
+      throw RainviewerAPIError.badURL
     }
     let request = URLRequest(url: url)
     let (data, response) = try await URLSession.shared.data(for: request)
     
-    guard (response as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching Rainviewer API Data")}
-    let rainviewerData = try JSONDecoder().decode(RainviewerSchema.self, from: data)
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw RainviewerAPIError.badResponse
+    }
+    
+    guard let rainviewerData = try? JSONDecoder().decode(RainviewerSchema.self, from: data) else {
+      throw RainviewerAPIError.badDecode
+    }
+    
     return rainviewerData
   }
 }
