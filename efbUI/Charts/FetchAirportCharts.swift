1cbb60ca78bba14327eec2f84bd28fa17e56abec
bradleypregon <bradley.pregon@gmail.com>
2024-11-17 11:00:39 -0600

    renamed class and improved fetch calls to use async/await pattern and localized error descriptions

--- efbUI/Charts/FetchAirportCharts.swift
+++ efbUI/Charts/FetchAirportCharts.swift
@@ -7,26 +7,41 @@
 
 import Foundation
 
-class FetchAirportCharts {
-  // https://api.aviationapi.com/v1/charts?apt=klax&group=1
-  func fetchCharts(icao: String, completion: @escaping (DecodedArray<AirportChartAPISchema>) -> ()) {
-    
-    let chartURL = "https://api.aviationapi.com/v1/charts?apt=\(icao)&group=1"
-    guard let url = URL(string: chartURL) else { return }
-    
-    URLSession.shared.dataTask(with: url) { (data, response, error) in
-      guard error == nil else { return }
-      guard let data = data else { return }
-      
-      do {
-        let result = try JSONDecoder().decode(DecodedArray<AirportChartAPISchema>.self, from: data)
-        DispatchQueue.main.async {
-          completion(result)
-        }
-      } catch let error {
-        print("Error fetching \(icao) Charts: \(error)")
-      }
-      
-    }.resume()
+enum AirportChartAPIError: Error, LocalizedError {
+  case badURL
+  case badResponse
+  case badDecode
+  
+  var errorDescription: String? {
+    switch self {
+    case .badURL:
+      return "Airport Chart API Error: Invalid URL."
+    case .badResponse:
+      return "Airport Chart API Error: Invalid Response."
+    case .badDecode:
+      return "Airport Chart API Error: Invalid Decode."
+    }
   }
+}
+
+class AirportChartAPI {
+  func fetchCharts(icao: String) async throws -> DecodedArray<AirportChartAPISchema> {
+    let url = "https://api.aviationapi.com/v1/charts?apt=\(icao)&group=1"
+    guard let url = URL(string: url) else {
+      throw AirportChartAPIError.badURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw AirportChartAPIError.badResponse
+    }
+    
+    guard let chartData = try? JSONDecoder().decode(DecodedArray<AirportChartAPISchema>.self, from: data) else {
+      throw AirportChartAPIError.badDecode
+    }
+    
+    return chartData
+  }
+  
 }
