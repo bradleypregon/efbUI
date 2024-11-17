189264f7fa5acf85cabf9fb183d95ad9d347742f
bradleypregon <bradley.pregon@gmail.com>
2024-11-17 11:03:05 -0600

    New class name, API calls utilize async/await patterns with custom error localized descriptions

--- efbUI/Weather/FAA/METAR/FetchAirportWx.swift
+++ efbUI/Weather/FAA/METAR/FetchAirportWx.swift
@@ -7,41 +7,61 @@
 
 import Foundation
 
-class FetchAirportWx {
-  func fetchMetar(icao: String, completion: @escaping ([AirportMETARSchema]) -> ()) {
-    // https://aviationweather.gov/cgi-bin/data/metar.php?ids=KDSM&format=json
-    let url = "https://aviationweather.gov/cgi-bin/data/metar.php?ids=\(icao)&format=json"
-    guard let url = URL(string: url) else { return }
-    
-    URLSession.shared.dataTask(with: url) { (data, response, error) in
-      guard error == nil else { return }
-      guard let data = data else { return }
-      do {
-        let result = try JSONDecoder().decode([AirportMETARSchema].self, from: data)
-        DispatchQueue.main.async {
-          completion(result)
-        }
-      } catch let error {
-        print("Error fetching \(icao) METAR: \(error)")
-      }
-      
-    }.resume()
-  }
+enum AirportWxError: Error, LocalizedError {
+  case invalidURL
+  case invalidResponse
+  case invalidDecode
   
-  func fetchTAF(icao: String, completion: @escaping ([String]) -> ()) {
-    let url = "https://aviationweather.gov/cgi-bin/data/taf.php?ids=\(icao)"
-    guard let url = URL(string: url) else { return }
+  var errorDescription: String? {
+    switch self {
+    case .invalidURL:
+      return "Airport Wx API Error: Invalid URL."
+    case .invalidResponse:
+      return "Airport Wx API Error: Invalid Response."
+    case .invalidDecode:
+      return "Airport Wx API Error: Invalid Decode."
+    }
+  }
+}
+
+class AirportWxAPI {
+  
+  func fetchMetar(icao: String) async throws -> [AirportMETARSchema] {
+    let url = "https://aviationweather.gov/cgi-bin/data/metar.php?ids=\(icao)&format=json"
+    guard let url = URL(string: url) else {
+      throw AirportWxError.invalidURL
+    }
     
-    URLSession.shared.dataTask(with: url) { (data, response, error) in
-      guard error == nil else { return }
-      guard let data = data else { return }
-      
-      if let result = String(data: data, encoding: .utf8)?.components(separatedBy: .newlines) {
-        DispatchQueue.main.async {
-          completion(result)
-        }
-      }
-    }.resume()
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw AirportWxError.invalidResponse
+    }
+    
+    guard let metars = try? JSONDecoder().decode([AirportMETARSchema].self, from: data) else {
+      throw AirportWxError.invalidDecode
+    }
+    
+    return metars
+  }
+
+  func fetchTAF(icao: String) async throws -> [String] {
+    let url = "https://aviationweather.gov/cgi-bin/data/taf.php?ids=\(icao)"
+    guard let url = URL(string: url) else {
+      throw AirportWxError.invalidURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw AirportWxError.invalidResponse
+    }
+    
+    guard let tafs = String(data: data, encoding: .utf8)?.components(separatedBy: .newlines) else {
+      throw AirportWxError.invalidDecode
+    }
+    
+    return tafs
   }
   
 }
