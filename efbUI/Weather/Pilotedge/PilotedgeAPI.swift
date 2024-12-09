c07da3204c19c6f2b66dba47683d7b4397120dab
bradleypregon <bradley.pregon@gmail.com>
2024-12-08 21:59:54 -0600

    async implementation

--- efbUI/Weather/Pilotedge/PilotedgeAPI.swift
+++ efbUI/Weather/Pilotedge/PilotedgeAPI.swift
@@ -7,7 +7,43 @@
 
 import Foundation
 
+enum PilotedgeAPIError: Error, LocalizedError {
+  case invalidURL
+  case invalidResponse
+  case invalidDecode
+  
+  var errorDescription: String? {
+    switch self {
+    case .invalidURL:
+      return "Invalid URL"
+    case .invalidResponse:
+      return "Invalid Response"
+    case .invalidDecode:
+      return "Invalid Decode"
+    }
+  }
+}
+
 class PilotedgeAPI {
+  func fetchATIS(icao: String) async throws -> [String] {
+    let url = "https://www.pilotedge.net/atis/\(icao).txt"
+    guard let url = URL(string: url) else {
+      throw PilotedgeAPIError.invalidURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw PilotedgeAPIError.invalidResponse
+    }
+    
+    guard let atis = String(data: data, encoding: .utf8)?.components(separatedBy: .newlines) else {
+      throw PilotedgeAPIError.invalidDecode
+    }
+    
+    return atis
+  }
+  
   func fetchATIS(icao: String, completion: @escaping ([String]) -> ()) {
     let url = "https://www.pilotedge.net/atis/\(icao).txt"
     guard let url = URL(string: url) else { return }
