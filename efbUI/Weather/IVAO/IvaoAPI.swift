bf1abb388c8c4ebcf2ad30967a4f8904cc466cfe
bradleypregon <bradley.pregon@gmail.com>
2024-12-08 21:59:11 -0600

    Get actual data from IVAO api

--- efbUI/Weather/IVAO/IvaoAPI.swift
+++ efbUI/Weather/IVAO/IvaoAPI.swift
@@ -6,13 +6,13 @@
 //
 
 import Foundation
-// TODO: Handle 500, 404, etc statusCodes
+// TODO: Handle data
 
 class IvaoAPI {
   func fetchATIS(icao: String) async throws -> String {
     let endpoint = ""
     guard let url = URL(string: endpoint) else { throw IVAOError.badURL }
-    let (data, response) = try await URLSession.shared.data(from: url)
+    let (_, response) = try await URLSession.shared.data(from: url)
     guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
       throw IVAOError.badResponse
     }
@@ -22,7 +22,7 @@ class IvaoAPI {
   func fetchMETAR(icao: String) async throws -> String {
     let endpoint = ""
     guard let url = URL(string: endpoint) else { throw IVAOError.badURL }
-    let (data, response) = try await URLSession.shared.data(from: url)
+    let (_, response) = try await URLSession.shared.data(from: url)
     guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
       throw IVAOError.badResponse
     }
@@ -32,7 +32,7 @@ class IvaoAPI {
   func fetchTAF(icao: String) async throws -> String {
     let endpoint = ""
     guard let url = URL(string: endpoint) else { throw IVAOError.badURL }
-    let (data, response) = try await URLSession.shared.data(from: url)
+    let (_, response) = try await URLSession.shared.data(from: url)
     guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
       throw IVAOError.badResponse
     }
