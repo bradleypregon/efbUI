6cf1668ed975001869cecfa08d554923718c5183
bradleypregon <bradley.pregon@gmail.com>
2024-12-30 00:04:46 -0600

    Better Toast notifications

--- efbUI/Toast/Toast.swift
+++ efbUI/Toast/Toast.swift
@@ -15,6 +15,8 @@ struct Toast: Identifiable {
   init(@ViewBuilder content: @escaping (String) -> some View) {
     self.content = .init(content(id))
   }
+  
+  var offsetX: CGFloat = 0
 }
 
 extension View {
@@ -22,5 +24,83 @@ extension View {
   func toast(_ toasts: Binding<[Toast]>) -> some View {
     self
       .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .overlay(alignment: .bottom) {
+        ToastsView(toasts: toasts)
+      }
+  }
+}
+
+fileprivate struct ToastsView: View {
+  @Binding var toasts: [Toast]
+  @State private var isExpanded: Bool = false
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      if isExpanded {
+        Rectangle()
+          .fill(.ultraThinMaterial)
+          .ignoresSafeArea()
+          .onTapGesture { isExpanded = false }
+      }
+      let layout = isExpanded ? AnyLayout(VStackLayout(spacing: 10)) : AnyLayout(ZStackLayout())
+      
+      layout {
+        ForEach($toasts) { $toast in
+          let index = (toasts.count - 1) - (toasts.firstIndex(where: { $0.id == toast.id }) ?? 0)
+          
+          toast.content
+            .offset(x: toast.offsetX)
+            .gesture(
+              DragGesture()
+                .onChanged { value in
+                  let xOffset = value.translation.width < 0 ? value.translation.width : 0
+                  toast.offsetX = xOffset
+                }.onEnded { value in
+                  let xOffset = value.translation.width + (value.velocity.width / 2)
+                  if -xOffset > 300 {
+                    $toasts.delete(toast.id)
+                  } else {
+                    withAnimation {
+                      toast.offsetX = 0
+                    }
+                  }
+                }
+            )
+            .visualEffect { [isExpanded] content, proxy in
+              content
+                .scaleEffect(isExpanded ? 1 : scale(index), anchor: .bottom)
+                .offset(y: isExpanded ? 0 : offsetY(index))
+            }
+            .frame(maxWidth: .infinity)
+            .transition(.asymmetric(insertion: .offset(y: 100), removal: .push(from: .top)))
+        }
+      }
+      .onTapGesture {
+        isExpanded.toggle()
+      }
+      .padding(.bottom, 15)
+    }
+    .animation(.bouncy, value: isExpanded)
+    .onChange(of: toasts.isEmpty) { _, newVal in
+      if newVal {
+        isExpanded = false
+      }
+    }
+  }
+  
+  nonisolated func offsetY(_ index: Int) -> CGFloat {
+    let offset = min(CGFloat(index) * 15, 30)
+    return -offset
+  }
+  nonisolated func scale(_ index: Int) -> CGFloat {
+    let scale = min(CGFloat(index) * 0.1, 1)
+    return 1 - scale
+  }
+}
+
+extension Binding<[Toast]> {
+  func delete(_ id: String) {
+    withAnimation(.bouncy) {
+      self.wrappedValue.removeAll(where: { $0.id == id })
+    }
   }
 }
--- efbUI/Toast/ToastTestContentView.swift
+++ efbUI/Toast/ToastTestContentView.swift
@@ -11,35 +11,46 @@ struct ToastTestContentView: View {
   @State var toasts: [Toast] = []
   var body: some View {
     Button {
-      showToast()
+      showToast(image: "airplane.departure", title: "20nm from KDSM: Final Call")
     } label: {
       Text("Toast")
     }
     .toast($toasts)
   }
   
-  func showToast() {
+  func showToast(image: String, title: String) {
     withAnimation(.bouncy) {
       let toast = Toast { id in
-        
+        ToastView(id, image: image, title: title)
       }
+      toasts.append(toast)
     }
   }
   
   @ViewBuilder
-  func ToastView(_ id: String) -> some View {
+  func ToastView(_ id: String, image: String, title: String) -> some View {
     HStack(spacing: 12) {
-      Image(systemName: "")
-      Text("Xmi from KDSM: Final Call")
+      Image(systemName: image)
+      Text(title)
         .font(.callout)
+        .fontWeight(.semibold)
       
       Button {
-        print("")
+        $toasts.delete(id)
       } label: {
         Image(systemName: "xmark.circle.fill")
           .font(.title2)
       }
     }
+    .foregroundStyle(.primary)
+    .padding(12)
+    .background {
+      Capsule()
+        .fill(.background)
+        .shadow(color: .black.opacity(0.06), radius: 3, x: -1, y: -3)
+        .shadow(color: .black.opacity(0.06), radius: 2, x: 1, y: 3)
+    }
+    .padding(.horizontal, 15)
   }
 }
 
