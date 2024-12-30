bece89e9cc4b2b21f88b1cdeb7346b0a342a7be5
bradleypregon <bradley.pregon@gmail.com>
2024-12-30 00:01:36 -0600

    Added new vars to SwiftData

--- efbUI/Data/SimBriefUserData.swift
+++ efbUI/Data/SimBriefUserData.swift
@@ -11,15 +11,20 @@ import SwiftData
 final class UserSettings {
   var simbriefUserID: String?
   var ownshipRegistration: String?
+  
   var outboundDistance: Int
+  var atisDistance: Int
   var inboundDistance: Int
+  var shortInboundDistance: Int
   var finalDistance: Int
   
-  init(simbriefUserID: String? = nil, ownshipRegistration: String? = nil, outboundDistance: Int = 20, inboundDistance: Int = 20, finalDistance: Int = 5) {
+  init(simbriefUserID: String? = nil, ownshipRegistration: String? = nil, outboundDistance: Int = 20, atisDistance: Int = 40, inboundDistance: Int = 20, shortInboundDistance: Int = 10, finalDistance: Int = 5) {
     self.simbriefUserID = simbriefUserID
     self.ownshipRegistration = ownshipRegistration
     self.outboundDistance = outboundDistance
+    self.atisDistance = atisDistance
     self.inboundDistance = inboundDistance
+    self.shortInboundDistance = shortInboundDistance
     self.finalDistance = finalDistance
   }
 }
