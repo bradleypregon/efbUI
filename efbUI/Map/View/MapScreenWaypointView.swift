12bfdb4bf1b43e8bf6b1ef7719ad02b5f669bd88
bradleypregon <bradley.pregon@gmail.com>
2025-01-06 08:41:15 -0600

    Waypoint view property update to use custom waypoints

--- efbUI/Map/View/MapScreenWaypointView.swift
+++ efbUI/Map/View/MapScreenWaypointView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct MapScreenWaypointView: View {
   @State private var popoverPresented: Bool = false
-  var wpt: OFPNavlog
+  var wpt: MyWaypoint
   
   var body: some View {
     Button {
       popoverPresented.toggle()
     } label: {
       VStack {
-        Image(systemName: wpt.ident == "TOC" || wpt.ident == "TOD" ? "bolt.horizontal.fill" : "triangle.fill")
-          .foregroundStyle(wpt.ident == "TOC" || wpt.ident == "TOD" ? .green : .blue)
-        Text(wpt.ident)
+        Image(systemName: "triangle.fill")
+          .foregroundStyle(.blue)
+        Text(wpt.identifier)
           .padding(6)
           .background(.blue)
           .foregroundStyle(.white)
@@ -28,19 +28,23 @@ struct MapScreenWaypointView: View {
     }
     .popover(isPresented: $popoverPresented) {
       List {
-        Text("Ident: \(wpt.ident)")
+//        Text("Ident: \(wpt.ident)")
+//        Text("Name: \(wpt.name)")
+//        Text("Type: \(wpt.type)")
+//        Text("Freq: \(wpt.frequency)")
+//        Text("Via: \(wpt.via)")
+//        Text("Alt: \(wpt.altitude)")
+//        Text("W/C: \(wpt.windComponent)")
+//        Text("Time leg: \(wpt.timeLeg)")
+//        Text("Time Total: \(wpt.timeTotal)")
+//        Text("Fuel Leg: \(wpt.fuelLeg)")
+//        Text("Fuel Total: \(wpt.fuelTotalUsed)")
+//        Text("Wind: \(wpt.windDir)/\(wpt.windSpd)")
+//        Text("Shear: \(wpt.shear)")
+        Text("Ident: \(wpt.identifier)")
         Text("Name: \(wpt.name)")
         Text("Type: \(wpt.type)")
-        Text("Freq: \(wpt.frequency)")
-        Text("Via: \(wpt.via)")
-        Text("Alt: \(wpt.altitude)")
-        Text("W/C: \(wpt.windComponent)")
-        Text("Time leg: \(wpt.timeLeg)")
-        Text("Time Total: \(wpt.timeTotal)")
-        Text("Fuel Leg: \(wpt.fuelLeg)")
-        Text("Fuel Total: \(wpt.fuelTotalUsed)")
-        Text("Wind: \(wpt.windDir)/\(wpt.windSpd)")
-        Text("Shear: \(wpt.shear)")
+        Text("Freq: \(000.000)")
       }
       .font(.caption)
       .listStyle(.plain)
