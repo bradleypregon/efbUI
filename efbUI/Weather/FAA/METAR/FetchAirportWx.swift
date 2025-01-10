9119573d527de7ecf675a66aafed4e73130bb6c5
bradleypregon <bradley.pregon@gmail.com>
2025-01-09 22:21:38 -0600

    Better error reporting, adding bounding box fetch function

--- efbUI/Weather/FAA/METAR/FetchAirportWx.swift
+++ efbUI/Weather/FAA/METAR/FetchAirportWx.swift
@@ -6,11 +6,13 @@
 //
 
 import Foundation
+import CoreLocation.CLLocation
 
 enum AirportWxError: Error, LocalizedError {
   case invalidURL
   case invalidResponse
-  case invalidDecode
+  case invalidDecode(Error)
+  case invalidStrings
   
   var errorDescription: String? {
     switch self {
@@ -18,8 +20,10 @@ enum AirportWxError: Error, LocalizedError {
       return "Airport Wx API Error: Invalid URL."
     case .invalidResponse:
       return "Airport Wx API Error: Invalid Response."
-    case .invalidDecode:
-      return "Airport Wx API Error: Invalid Decode."
+    case .invalidDecode(let message):
+      return "Airport Wx API Error: Invalid Decode. \(message)"
+    case .invalidStrings:
+      return "Airport Wx API Error: Invalid String Array Response."
     }
   }
 }
@@ -38,11 +42,53 @@ class AirportWxAPI {
       throw AirportWxError.invalidResponse
     }
     
-    guard let metars = try? JSONDecoder().decode([AirportMETARSchema].self, from: data) else {
-      throw AirportWxError.invalidDecode
+    do {
+      let metars = try JSONDecoder().decode([AirportMETARSchema].self, from: data)
+      return metars
+    } catch {
+      let rawResponse = String(data: data, encoding: .utf8)
+      throw AirportWxError.invalidDecode(error)
+    }
+  }
+  
+  func fetchMetar(icao: String, at time: Date) async throws -> [AirportMETARSchema] {
+    let url = "https://aviationweather.gov/cgi-bin/data/metar.php?ids=\(icao)&format=json"
+    guard let url = URL(string: url) else {
+      throw AirportWxError.invalidURL
     }
     
-    return metars
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw AirportWxError.invalidResponse
+    }
+    
+    do {
+      let metars = try JSONDecoder().decode([AirportMETARSchema].self, from: data)
+      return metars
+    } catch {
+      throw AirportWxError.invalidDecode(error)
+    }
+  }
+  
+  func fetchBBox(ne: CLLocationCoordinate2D, sw: CLLocationCoordinate2D) async throws -> [AirportMETARSchema] {
+    let url = "https://aviationweather.gov/api/data/metar?bbox=\(sw.latitude),\(sw.longitude),\(ne.latitude),\(ne.longitude)&format=json"
+    guard let url = URL(string: url) else {
+      throw AirportWxError.invalidURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+      throw AirportWxError.invalidResponse
+    }
+    
+    do {
+      let metars = try JSONDecoder().decode([AirportMETARSchema].self, from: data)
+      return metars
+    } catch let error {
+      throw AirportWxError.invalidDecode(error)
+    }
   }
 
   func fetchTAF(icao: String) async throws -> [String] {
@@ -58,9 +104,8 @@ class AirportWxAPI {
     }
     
     guard let tafs = String(data: data, encoding: .utf8)?.components(separatedBy: .newlines) else {
-      throw AirportWxError.invalidDecode
+      throw AirportWxError.invalidStrings
     }
-    
     return tafs
   }
   
