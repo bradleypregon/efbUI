de999ad9ab4570d1d3721bb0370d536830aad613
bradleypregon <bradley.pregon@gmail.com>
2025-01-09 22:26:12 -0600

    Fix function for rounding doubles

--- efbUI/Extensions.swift
+++ efbUI/Extensions.swift
@@ -12,8 +12,8 @@ import CoreLocation
 
 extension Double {
   func rounded(toPlaces places: Int) -> Double {
-    let divisor = pow(1.0, Double(places))
-    return (self * divisor).rounded()
+    let divisor = pow(10.0, Double(places))
+    return (self * divisor).rounded() / divisor
   }
 }
 
