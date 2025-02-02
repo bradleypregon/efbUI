23d75255fe04f341ee6dd1116bc7c1968f747abf
bradleypregon <bradley.pregon@gmail.com>
2025-02-02 09:52:26 -0600

    Tweak for new geojson, not needed

--- efbUI/Data/Airports/AirportSchema.swift
+++ efbUI/Data/Airports/AirportSchema.swift
@@ -28,3 +28,36 @@ enum AirportSizeSchema: String, Decodable {
   case medium = "Medium"
   case small = "Small"
 }
+
+struct AirportGeoSchema: Decodable {
+  let type: String
+  let features: [AirportSchemaFeature]
+  
+  enum CodingKeys: String, CodingKey {
+    case type, features
+  }
+}
+
+struct AirportSchemaFeature: Decodable, Identifiable {
+  var id: String = UUID().uuidString
+  let type: String
+  let coordinates: [Double]
+  let properties: AirportSchemaFeatureProperties
+  
+  enum CodingKeys: String, CodingKey {
+    case type, coordinates, properties
+  }
+}
+
+struct AirportSchemaFeatureProperties: Decodable {
+  let name: String
+  let city: String
+  let country: String
+  let countryCode: String
+  let icao: String
+  let size: AirportSizeSchema
+  
+  enum CodingKeys: String, CodingKey {
+    case name, city, country, countryCode, icao, size
+  }
+}
